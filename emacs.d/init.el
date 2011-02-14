9911b9da19a516991ca6647e5ac76bef70c8d683
Masahiro Koga <masahirokoga@gmail.com>
2011-02-14 16:43:57 +0900

    init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -882,3 +882,21 @@ and source-file directory for your debugger." t)
 ;; load init file for darwin
 (if (eq window-system 'ns)
     (load (expand-file-name "~/.emacs.d/darwin-init.el")))
+
+
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 2)
+ '(org-agenda-files nil t)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+;;
