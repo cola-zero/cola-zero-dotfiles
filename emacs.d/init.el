a4911bcc996999c96f90ffd78f7dfca2532a17ef
Masahiro Koga <masahirokoga@gmail.com>
2011-02-14 21:02:01 +0900

    comment out settings for multi-term. because delete-other-windows does not work right

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -499,16 +499,16 @@
 ;; multi-term
 ;; http://sakito.jp/emacs/emacsshell.html#emacs-shell
 ;; curl -O http://www.emacswiki.org/emacs/download/multi-term.el
-(defun skt:shell ()
-  (or (executable-find "zsh")
-      (executable-find "bash")
-      (executable-find "cmdproxy")
-      (error "can't find 'shell' command in PATH!!")))
-(setq shell-file-name (skt:shell))
-(setenv "SHELL" shell-file-name)
-(setq explicit-shell-file-name shell-file-name)
-(require 'multi-term)
-(setq milti-term-program shell-file-name)
+;; (defun skt:shell ()
+;;   (or (executable-find "zsh")
+;;       (executable-find "bash")
+;;       (executable-find "cmdproxy")
+;;       (error "can't find 'shell' command in PATH!!")))
+;; (setq shell-file-name (skt:shell))
+;; (setenv "SHELL" shell-file-name)
+;; (setq explicit-shell-file-name shell-file-name)
+;; (require 'multi-term)
+;; (setq milti-term-program shell-file-name)
 
 ;; indent with space
 (setq-default indent-tabs-mode nil)
