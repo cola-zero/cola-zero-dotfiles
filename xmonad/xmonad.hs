2bec03871fe02d86c5b7e12bb5b01fbcb8666309
Masahiro Koga <masahirokoga@gmail.com>
2011-02-28 23:11:22 +0900

    use ALT key as mod

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -35,7 +35,7 @@ myBorderWidth   = 1
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
 --
-myModMask       = mod4Mask
+myModMask       = mod1Mask
 
 -- The mask for the numlock key. Numlock status is "masked" from the
 -- current modifier status, so the keybindings will work with numlock on or
