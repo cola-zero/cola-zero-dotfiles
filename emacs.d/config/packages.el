e2aef35782c09e36af98ab8e2430703b0a6d9b8b
Masahiro Koga <masahirokoga@gmail.com>
2011-04-26 02:12:42 +0900

    comment out evernote-mode

--- emacs.d/config/packages.el
+++ emacs.d/config/packages.el
@@ -367,14 +367,14 @@
 
 
 ;; evernote-mode
-(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8"))
-(add-to-list 'load-path (concat package-base-dir "/evernote-mode"))
-(require 'evernote-mode)
-(global-set-key "\C-cec" 'evernote-create-node)
-(global-set-key "\C-ceo" 'evernote-open-note)
-(global-set-key "\C-ces" 'evernote-search-notes)
-(global-set-key "\C-ceS" 'evernote-do-saved-search)
-(global-set-key "\C-cew" 'evernote-write-note)
+;; (setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8"))
+;; (add-to-list 'load-path (concat package-base-dir "/evernote-mode"))
+;; (require 'evernote-mode)
+;; (global-set-key "\C-cec" 'evernote-create-node)
+;; (global-set-key "\C-ceo" 'evernote-open-note)
+;; (global-set-key "\C-ces" 'evernote-search-notes)
+;; (global-set-key "\C-ceS" 'evernote-do-saved-search)
+;; (global-set-key "\C-cew" 'evernote-write-note)
 
 
 
