2f5ce677911ea158040d05055a42cc576f516fc4
Masahiro Koga <masahirokoga@gmail.com>
2011-07-28 14:25:44 +0900

    add po-mode

--- emacs.d/config/packages.el
+++ emacs.d/config/packages.el
@@ -285,3 +285,11 @@
  '(("M-g" . e2wm:dp-magit))
  e2wm:prefix-key)
 
+;; po-mode
+(autoload 'po-mode "po-mode"
+  "Major mode for translators to edit PO files" t)
+(setq auto-mode-alist (cons '("\\.po\\'\\|\\.po\\." . po-mode)
+                            auto-mode-alist))
+(autoload 'po-find-file-coding-system "po-compat")
+(modify-coding-system-alist 'file "\\.po\\'\\|\\.po\\."
+                            'po-find-file-coding-system)
