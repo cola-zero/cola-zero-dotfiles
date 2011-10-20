49b3546c0333011aee54000e5d5f69c4493b9975
Masahiro Koga <masahirokoga@gmail.com>
2011-10-20 17:35:47 +0900

    use rbenv.

--- emacs.d/cola-zero/ruby-mode.el
+++ emacs.d/cola-zero/ruby-mode.el
@@ -1,5 +1,10 @@
 ;;ruby
 
+(add-to-list 'exec-path "~/.rbenv/bin")
+(setenv "PATH" (concat "~/.rbenv/bin:" (getenv "PATH")))
+(add-to-list 'exec-path "~/.rbenv/shims")
+(setenv "PATH" (concat "~/.rbenv/shims:" (getenv "PATH")))
+
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
