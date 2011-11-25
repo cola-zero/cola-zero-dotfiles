fec63052691f8f4f8e2ca7a75a6c2bafe5a4cce6
cola-zero <colazero@mail.mkoga.net>
2011-11-25 14:05:16 +0900

    add config for rsense mode.

--- emacs.d/cola-zero/rsense.el
+++ emacs.d/cola-zero/rsense.el
@@ -1,3 +1,7 @@
 (setq rsense-home (expand-file-name "~/.emacs.d/vendor/rsense"))
 (add-to-list 'load-path (concat rsense-home "/etc"))
 (require 'rsense)
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-rsense-method)
+            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
