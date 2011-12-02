c8727d2c73ee43a4c929c55edcbf5fb36c56fa32
cola-zero <colazero@mail.mkoga.net>
2011-12-03 01:16:50 +0900

    add paredit.el

---
+++ emacs.d/cola-zero/paredit.el
@@ -0,0 +1,4 @@
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interpraction-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
