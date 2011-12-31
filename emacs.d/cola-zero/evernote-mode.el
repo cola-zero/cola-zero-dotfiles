38ab17d7fd9c363a303142784f568b5add4fc912
cola-zero <colazero@mail.mkoga.net>
2011-12-31 21:46:59 +0900

    use evernote-mode

---
+++ emacs.d/cola-zero/evernote-mode.el
@@ -0,0 +1,9 @@
+(setq evernote-username "cola-zero") ; optional: you can use this username as default.
+(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8")) ; optional
+(global-set-key "\C-cec" 'evernote-create-note)
+(global-set-key "\C-ceo" 'evernote-open-note)
+(global-set-key "\C-ces" 'evernote-search-notes)
+(global-set-key "\C-ceS" 'evernote-do-saved-search)
+(global-set-key "\C-cew" 'evernote-write-note)
+(global-set-key "\C-cep" 'evernote-post-region)
+(global-set-key "\C-ceb" 'evernote-browser)
