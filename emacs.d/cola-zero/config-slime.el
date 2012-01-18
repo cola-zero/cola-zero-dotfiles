871ff948564ae007f3a94f4a401e9978f3193cf1
cola-zero <colazero@mail.mkoga.net>
2012-01-18 23:14:43 +0900

    update slime

--- emacs.d/cola-zero/config-slime.el
+++ emacs.d/cola-zero/config-slime.el
@@ -1,11 +1,13 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/el-get/slime"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/el-get/slime/contrib"))
-(require 'slime-autoloads)
-(require 'slime-fancy)
-(add-hook 'slime-load-hook (lambda ()
-                             (require 'slime-fancy)))
-
 (setq inferior-lisp-program "ccl")
-(eval-after-load "slime"
-   '(slime-setup '(slime-fancy slime-banner)))
-(setq slime-net-coding-system 'utf-8-unix)
+(require 'slime)
+(slime-setup '(slime-repl slime-fancy slime-banner))
+;; (require 'slime-autoloads)
+;; (require 'slime-fancy)
+;; (add-hook 'slime-load-hook (lambda ()
+;;                              (require 'slime-fancy)))
+
+;; (eval-after-load "slime"
+;;    '(slime-setup '(slime-fancy slime-banner)))
+;; (setq slime-net-coding-system 'utf-8-unix)
