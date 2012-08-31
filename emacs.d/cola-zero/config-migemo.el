b6a5970e7b1e6d4c3f89d2a618a24ee7dcdc71f2
cola-zero <colazero@mail.mkoga.net>
2012-08-31 21:07:54 +0900

    use euc-jp on linux

--- emacs.d/cola-zero/config-migemo.el
+++ emacs.d/cola-zero/config-migemo.el
@@ -3,10 +3,13 @@
 (setq migemo-options '("-q" "--emacs"))
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
-(setq migemo-coding-system 'utf-8-unix)
 (if (eq system-type 'darwin)
-    (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-  (setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+    (progn
+      (setq migemo-coding-system 'utf-8-unix)
+      (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"))
+  (progn
+    (setq migemo-coding-system 'euc-jp-unix)
+    (setq migemo-dictionary "/usr/share/migemo/migemo-dict"))
   )
 (load-library "migemo")
 (migemo-init)
