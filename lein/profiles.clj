a86404c166c4ca03650bd5f29084f6a4462fc9e8
cola-zero <colazero@mail.mkoga.net>
2012-09-12 08:02:08 +0900

    update ritz plugin

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user
- {:plugins [[lein-ritz "0.4.0"]
+ {:plugins [[lein-ritz "0.4.2"]
             [lein-pprint "1.1.1"]
             [lein-noir "1.2.1"]
             [lein-midje "2.0.0-SNAPSHOT"]]}
